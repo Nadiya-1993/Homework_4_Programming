f18327e8c7f6313e9d83db1c3837a77fcb53d986
Nadiya <nadiyanzmp@gmail.com>
2022-07-26 12:08:11 +0400

    homework

---
+++ task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+ Console.WriteLine("Введите число A: ");
+  int numberA = int.Parse(Console.ReadLine());
+  Console.WriteLine("Введите число B: ");
+  int numberB = int.Parse(Console.ReadLine());
+
+int Exponent (int numberA, int numberB)
+{
+  int result = 1;
+  for (int i=1; i <= numberB; i++)
+    {
+    result = result * numberA;
+    }
+    return result;
+}
+  int exponentresult = Exponent(numberA, numberB);
+  Console.WriteLine(exponentresult);
---
+++ task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число N: ");
+int Number = int.Parse(Console.ReadLine());
+
+  int SumNumber(int N)
+  {
+    int cont = Convert.ToString(Number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < cont; i++)
+    {
+      advance = Number - Number % 10;
+      result = result + (Number - advance);
+      Number = Number / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(Number);
+Console.WriteLine(sumNumber);
